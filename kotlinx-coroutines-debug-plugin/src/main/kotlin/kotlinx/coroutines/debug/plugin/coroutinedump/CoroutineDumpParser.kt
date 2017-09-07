698274e02a3d8b59ce6d337116fd40a18a5e19f4
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2017-09-07 11:39:42 +0300

    plugin: show coroutine in "created" state

--- kotlinx-coroutines-debug-plugin/src/main/kotlin/kotlinx/coroutines/debug/plugin/coroutinedump/CoroutineDumpParser.kt
+++ kotlinx-coroutines-debug-plugin/src/main/kotlin/kotlinx/coroutines/debug/plugin/coroutinedump/CoroutineDumpParser.kt
@@ -4,7 +4,7 @@ package kotlinx.coroutines.debug.plugin.coroutinedump
  * @author Kirill Timofeev
  */
 object CoroutineDumpParser {
-    private val nameAndStatus = "^\\\"(.+)\\\"\\s.*\n {2}Status:\\s([A-Za-z]+)(.*)\n.*".toRegex()
+    private val nameAndStatus = "^\\\"(.+)\\\".*\n {2}Status:\\s([A-Za-z]+)(.*)\n.*".toRegex()
     fun parse(coroutineDump: String) =
             coroutineDump.drop(coroutineDump.indexOf('\n') + 1).split("\n\n").map {
                 val (_, name, status, additionalInfo) = nameAndStatus.find(it)?.groupValues ?: return@map null
--- kotlinx-coroutines-debug-plugin/src/main/kotlin/kotlinx/coroutines/debug/plugin/coroutinedump/CoroutineState.kt
+++ kotlinx-coroutines-debug-plugin/src/main/kotlin/kotlinx/coroutines/debug/plugin/coroutinedump/CoroutineState.kt
@@ -6,10 +6,11 @@ import javax.swing.Icon
 /**
  * @author Kirill Timofeev
  */
-sealed class Status(open val name: String, open val code: Int, open val icon: Icon) {
+sealed class Status(val name: String, val code: Int, val icon: Icon) {
     companion object {
         fun byName(name: String) = when (name.toLowerCase()) {
             "suspended" -> Suspended
+            "created" -> Created
             "running" -> Running
             else -> throw IllegalArgumentException("Unknown status name: $name")
         }
@@ -17,6 +18,7 @@ sealed class Status(open val name: String, open val code: Int, open val icon: Ic
 }
 
 object Suspended : Status("Suspended", 0, AllIcons.Debugger.ThreadStates.Paused)
-object Running : Status("Running", 1, AllIcons.Debugger.ThreadStates.Running)
+object Created : Status("Created", 1, AllIcons.Debugger.ThreadStates.Paused)
+object Running : Status("Running", 2, AllIcons.Debugger.ThreadStates.Running)
 
 data class CoroutineState(val name: String, val status: Status, val stack: String, val additionalInfo: String)
