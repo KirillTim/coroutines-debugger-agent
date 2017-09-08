609b67fdf0e25199d4cc2c80b95954d74919f91e
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2017-09-08 16:50:54 +0300

    DSL for test checkers

---
+++ kotlinx-coroutines-debug-agent-test/src/test/kotlin/kotlinx/coroutines/debug/test/CoroutineMatcherDSL.kt
@@ -0,0 +1,96 @@
+package kotlinx.coroutines.debug.test
+
+import kotlinx.coroutines.debug.manager.CoroutineInfoItem
+import java.util.regex.Pattern
+
+/**
+ * @author Kirill Timofeev
+ */
+private val Any?.p: String
+    get() = if (this == null) ".*" else "$this".q
+
+private val String.q: String
+    get() = Pattern.quote(this)
+
+sealed class RegexHolder(val regex: String)
+
+sealed class StackItem(regex: String) : RegexHolder(regex)
+data class CoroutineMarker(val name: String, val current: Boolean = false)
+    : StackItem("${CoroutineInfoItem(name, current)}".q)
+
+data class StackElement(val method: String?, val file: String? = null, val line: Int? = null)
+    : StackItem(" {4}at ${method.p}\\(${file.p}${if (line == null) ".*" else ":${line.p}"}\\)")
+
+sealed class Status(status: String, additionalInfo: String = "") : RegexHolder(" {2}Status: $status $additionalInfo")
+data class Suspended(val atMethod: String? = null) : Status("Suspended", "at ${atMethod.p}")
+object Created : Status("Created")
+data class Running(val onThread: String? = null) : Status("Running", "on ${onThread.p}")
+
+data class RegexStringMatcher(val string: String) : Matchable {
+    constructor(stackElement: RegexHolder) : this(stackElement.regex)
+
+    override fun match(other: Matchable) =
+            other is RegexStringMatcher && string.toRegex().matchEntire(other.string) != null
+}
+
+abstract class WithMatchablePattern<T : Matchable> {
+    abstract val pattern: MutableList<Token<T>>
+    fun match(data: List<T>) = matches(pattern, data)
+}
+
+abstract class FullStringMatcher : WithMatchablePattern<RegexStringMatcher>() {
+    fun matchEntireString(string: String) = match(string.split('\n').map { RegexStringMatcher(it) })
+}
+
+class Stack : FullStringMatcher() {
+    val any = StackElement(null)
+    override val pattern = mutableListOf<Token<RegexStringMatcher>>()
+    fun Int.methods(element: StackElement) =
+            repeat(this) { pattern += Single(RegexStringMatcher(element)) }
+
+    fun Int.methods(method: String?, file: String? = null, line: Int? = null) =
+            methods(StackElement(method, file, line))
+
+    fun marker(name: String, current: Boolean = false) {
+        pattern += Single(RegexStringMatcher(CoroutineMarker(name, current)))
+    }
+
+    fun method(element: StackElement) {
+        pattern += Single(RegexStringMatcher(element))
+    }
+
+    fun method(method: String?, file: String? = null, line: Int? = null) =
+            method(StackElement(method, file, line))
+
+
+    fun zeroOrMore(element: StackElement) {
+        pattern += ZeroOrMore(RegexStringMatcher(element))
+    }
+
+    fun zeroOrMore(method: String?, file: String? = null, line: Int? = null) =
+            zeroOrMore(StackElement(method, file, line))
+
+    fun oneOrMore(element: StackElement) {
+        pattern += OneOrMore(RegexStringMatcher(element))
+    }
+
+    fun oneOrMore(method: String?, file: String? = null, line: Int? = null) =
+            oneOrMore(StackElement(method, file, line))
+}
+
+class Coroutine(val name: String, val status: Status) : FullStringMatcher() {
+    override val pattern: MutableList<Token<RegexStringMatcher>>
+            = mutableListOf(Single(RegexStringMatcher("\\\"$name\\\".*")), Single(RegexStringMatcher(status)))
+}
+
+fun coroutine(name: String, status: Status, stack: Coroutine.() -> Stack): Coroutine {
+    val coroutine = Coroutine(name, status)
+    coroutine.pattern += coroutine.stack().pattern
+    return coroutine
+}
+
+fun stack(append: Stack.() -> Unit): Stack {
+    val stack = Stack()
+    stack.append()
+    return stack
+}
---
+++ kotlinx-coroutines-debug-agent-test/src/test/kotlin/kotlinx/coroutines/debug/test/GenericRegexMatcher.kt
@@ -0,0 +1,96 @@
+package kotlinx.coroutines.debug.test
+
+import org.junit.Test
+import java.util.*
+import kotlin.system.measureNanoTime
+
+/**
+ * @author Kirill Timofeev
+ */
+interface Matchable {
+    fun match(other: Matchable): Boolean
+}
+
+sealed class Token<out T : Matchable> {
+    abstract val symbol: T
+}
+
+data class Single<out T : Matchable>(override val symbol: T) : Token<T>()
+data class ZeroOrMore<out T : Matchable>(override val symbol: T) : Token<T>()
+data class OneOrMore<out T : Matchable>(override val symbol: T) : Token<T>()
+
+fun <T : Matchable> matches(pattern: List<Token<T>>, data: List<T>): Boolean {
+    if (pattern.isEmpty()) return data.isEmpty()
+    if (pattern.isNotEmpty() && data.isEmpty()) return false
+    val head = pattern.first()
+    when (head) {
+        is Single -> {
+            return if (head.symbol.match(data.first())) matches(pattern.drop(1), data.drop(1))
+            else false
+        }
+        is ZeroOrMore -> {
+            return if (head.symbol.match(data.first()))
+                matches(pattern, data.drop(1)) || matches(pattern.drop(1), data.drop(1))
+            else matches(pattern.drop(1), data)
+        }
+        is OneOrMore -> {
+            return if (head.symbol.match(data.first()))
+                matches(pattern, data.drop(1)) || matches(pattern.drop(1), data.drop(1))
+            else false
+        }
+    }
+}
+
+class GenericRegexMatcherTest {
+    private fun generateRandomSkeleton(size: Int) = (1..size).map { 'a' + Random().nextInt(10) }
+
+    private fun generatePattern(skeleton: List<Char>)
+            = skeleton.joinToString(separator = "") { "$it${if (Random().nextBoolean()) '*' else '+'}" }
+
+    private fun generateString(skeleton: List<Char>, maxLetters: Int) =
+            skeleton.joinToString(separator = "") { "$it".repeat(Random().nextInt(maxLetters) + 1) }
+
+    private data class MatchableStr(val value: String) : Matchable {
+        override fun match(other: Matchable) = other is MatchableStr && other.value == value
+    }
+
+    private fun str2Matchable(str: String): List<Token<MatchableStr>> {
+        val res = mutableListOf<Token<MatchableStr>>()
+        var i = 0
+        while (i <= str.lastIndex) {
+            if (i < str.lastIndex && str[i + 1] == '*') {
+                res += ZeroOrMore(MatchableStr(str[i].toString()))
+                i++
+            } else if (i < str.lastIndex && str[i + 1] == '+') {
+                res += OneOrMore(MatchableStr(str[i].toString()))
+                i++
+            } else res += Single(MatchableStr(str[i].toString()))
+            i++
+        }
+        return res
+    }
+
+    private fun stress(size: Int, maxLetters: Int, runs: Int): Double {
+        val avg = mutableListOf<Double>()
+        (1..runs).forEach {
+            val ske = generateRandomSkeleton(size)
+            val patternStr = generatePattern(ske)
+            val str = generateString(ske, maxLetters)
+            val patternMy = str2Matchable(patternStr)
+            val strMy = str.map { MatchableStr(it.toString()) }
+            var resultRegex = false
+            var timeRegex = measureNanoTime { resultRegex = patternStr.toRegex().matchEntire(str) != null }
+            if (timeRegex == 0L) timeRegex = 1L
+            var resultMe = false
+            val timeMe = measureNanoTime { resultMe = matches(patternMy, strMy) }
+            require(resultRegex == resultMe)
+            avg += timeMe.toDouble() / timeRegex.toDouble()
+        }
+        return avg.average()
+    }
+
+    @Test
+    fun stressTest() {
+        println(stress(100, 100, 50))
+    }
+}
